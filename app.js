601070cd8ea327b579219f6674f5ae35688a4900
Dongwook Lim <ldu9905@gmail.com>
2022-04-13 20:20:52 +0900

    JS: Canvas painting

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const INITIAL_COLOR = '#202124';
+
+canvas.width = 700;
+canvas.height = 900;
+
 let painting = false;
 
-ctx.strokeStyle = 'black';
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
 
 function startPainting() {
   painting = true;
@@ -16,8 +22,8 @@ function stopPainting() {
 function handleMouseMove(event) {
   let x = event.offsetX;
   let y = event.offsetY;
-  if (painting === false) {
-    console.log(x, y);
+
+  if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
@@ -29,6 +35,6 @@ function handleMouseMove(event) {
 if (canvas) {
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
-  canvas.addEventListener('mouseout', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
   canvas.addEventListener('mousemove', handleMouseMove);
 }
